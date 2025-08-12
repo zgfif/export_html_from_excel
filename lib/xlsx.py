520979e37f297e061643830a9812c1f8702cedb4
zgfif <zihzag@gmail.com>
2025-08-12 23:08:21 +0300

    add comments to Xlsx class

--- lib/xlsx.py
+++ lib/xlsx.py
@@ -10,21 +10,21 @@ class Xlsx:
 
     def column_names(self) -> list[str]:
         """Return the list of Excel column names."""
-        # Выбрать лист
-        sheet = self._work_book.active  # или wb["ИмяЛиста"]
+        # select excel file sheet
+        sheet = self._work_book.active  # or wb["sheet_name"]
 
         reader = sheet.iter_rows(values_only=True)
-        row = next(reader, None)
+        row = next(reader, None) # this row contains column names
 
-        return [cell for cell in row if cell]
+        return [cell for cell in row if cell]  # remove empty column names and return the list of column names.
 
 
     def data_rows(self) ->list[list[str]]:
         """Return all the rows from Excel file."""
         data = []
-        sheet = self._work_book.active
-        reader = sheet.iter_rows(values_only=True)
-        next(reader, None)
+        sheet = self._work_book.active # we have only one sheet so choose it
+        reader = sheet.iter_rows(values_only=True) # generator of excel file
+        next(reader, None) # skip the row with column names
         for row in reader:
             data.append(row)
         
